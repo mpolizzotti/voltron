930b1415b8ee7bf020d501d1bf51bfb86041b7b4
Matthew Polizzotti <mpolizzotti@unicon.net>
2013-06-12 13:24:50 -0700

    Simplified config.js file. Removed excess code that was not being used.

--- config.js
+++ config.js
@@ -3,21 +3,13 @@
 // The purpose of this file is to setup the use
 // of a configuration file for our node development server.
 
-// Modules.
+// Modules & Variables.
 var nconf = require('nconf'),
-	fs = require('fs');
-
-// Configuration object.
-var config = {};
+	fs = require('fs'),
+	config = {};
 
 // Setup nconf to use (in-order) command-line arguments and environment variables.
 nconf.argv().env();
-
-// NODE_ENV is an environment variable that can be defined and set
-// when the server is started (i.e., NODE_ENV=production node app.js).
-// This is currently not leveraged in this implementation but could
-// be used to set specific production or development configuration files.
-var NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Define configuration path.
 config.path = __dirname + '/config';
